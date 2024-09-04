c6b4e2adf371f8a4b02e5f321860979ecaed2ae9
zameer <zameeralucknowi@gmail.com>
2024-09-05 04:59:15 +0530

    changed static

--- index.js
+++ index.js
@@ -17,14 +17,13 @@ dotenv.config();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api/auth',authRoute);
 app.use('/api/user',userRoute);
 app.use('/api/products',productRoute)
 app.use('/api/orders',orderRoute)
 app.use('/api/carts',cartRoute)
 app.use('/api/checkout',paypalRoute)
-
-app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle all other routes by serving index.html
 app.get('*', (req, res) => {

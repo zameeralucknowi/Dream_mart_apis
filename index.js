d1c97e673a7c6829a77d0950d65b1681782ad58d
zameer <zameeralucknowi@gmail.com>
2024-09-05 04:13:31 +0530

    serving html

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/checkout',paypalRoute)
 
 app.use(express.static(path.join(__dirname,'client/build')));
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 mongoose.connect(process.env.MONGO_URL)
